57567f2e5dee94f14688fe5db4fe003b20f04b3a
Battle Chicken <felix@larkov.de>
2022-04-29 21:53:41 -0400

    EEE

---
+++ config/NvChad/custom/plugins/init.lua
@@ -0,0 +1,13 @@
+return {
+["jose-elias-alvarez/null-ls.nvim"] = {      after = "nvim-lspconfig",
+config = function()
+  require("custom.plugins.null-ls").setup()
+end,
+},
+["williamboman/nvim-lsp-installer"] = {
+  after = "nvim-lspconfig",
+  config = function()
+    require(custom.plugins.lspconfig).setup()
+  end,
+},
+}
